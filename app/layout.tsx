ca2f38bd32e232cf9146bba835132fd270a68c33
ole-froiland <olealexanderfroiland02@gmail.com>
2026-05-25 11:13:17 +0200

    Suppress html hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
         <script dangerouslySetInnerHTML={{ __html: languageScript }} />
